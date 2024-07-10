d228049ee5a249aea13af944c19f067f4dae662f
rakibmiah99 <rakibmiah120199@gmail.com>
2024-07-10 15:01:51 +0600

    jfk

--- resources/views/topic_data.blade.php
+++ resources/views/topic_data.blade.php
@@ -8,20 +8,24 @@
 <div class="mt-3 row">
 
 
-    <div class="plyr__video-embed" id="player">
+    {{--<div class="plyr__video-embed" id="player">
         <iframe
             src="https://www.youtube.com/embed/_cvZXVmEZ3M?si=mp-rao2M0nX5Etk-&amp"
             allowfullscreen
             allowtransparency
             allow="autoplay"
         ></iframe>
-    </div>
+    </div>--}}
 
-    @foreach($topic->topic_data as $topic)
+    @foreach(/*$topic->topic_data*/ [
+    (object)['id' => 1, 'emb_id' => '_cvZXVmEZ3M'],
+    (object)['id' => 2, 'emb_id' => '1hEjlr3Vs-o'],
+    (object)['id' => 3, 'emb_id' => 'Lpu666ClDCk'],
+] as $topic)
         <div class="col-12 col-lg-6 col-xl-3">
             <div class="p-2">
                 <div class="card">
-                    <div class="player" id="player-{{$topic->id}}" data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+                    <div class="player" id="player-{{$topic->id}}" data-plyr-provider="youtube" data-plyr-embed-id="{{$topic->emb_id}}"></div>
                 </div>
             </div>
         </div>
